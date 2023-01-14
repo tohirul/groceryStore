be90dafaac3ab0f3e3d9b8c5a2657f9a04a71bf1
Tohirul Islam <tohirul.islam164@gmail.com>
2023-01-14 22:34:50 +0600

    Blogs Completed

--- main.js
+++ main.js
@@ -61,11 +61,32 @@ userBtn.onclick = () => {
 	overlay.classList.remove("active");
 };
 // @ts-ignore
-const swiper = new Swiper(".product-slider", {
+const productSwiper = new Swiper(".product-slider", {
 	loop: true,
 	spaceBetween: 10,
 	autoplay: {
-		delay: 2000,
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	centeredSlides: true,
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		1024: {
+			slidesPerView: 3,
+		},
+	},
+});
+// @ts-ignore
+const reviewSwiper = new Swiper(".review-slider", {
+	loop: true,
+	spaceBetween: 10,
+	autoplay: {
+		delay: 3000,
 		disableOnInteraction: false,
 	},
 	centeredSlides: true,
